de24c28ca227a6bccacbfce191900851bb8b5867
Débora Picado <dbpicado@gmail.com>
2021-03-12 16:48:06 +0000

    Actualizacion Fetch

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -55,3 +55,32 @@ export function Home() {
 		</div>
 	);
 }
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify([
+  {
+    "label": "Make the bed",
+    "done": false
+  },
+  {
+    "label": "Walk the dog",
+    "done": false
+  },
+  {
+    "label": "Do the replits",
+    "done": false
+  }
+]);
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://assets.breatheco.de/apis/fake/todos/user/debpicado", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
